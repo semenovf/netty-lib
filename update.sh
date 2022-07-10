b2c8d39e458a9fb73095d311776ef6855ef8157b
Semenov Fedor <fedor.v.semenov@gmail.com>
2022-07-10 18:37:38 +0500

    Updated `update.sh`.

--- update.sh
+++ update.sh
@@ -5,12 +5,12 @@ CEREAL_RELEASE=v1.3.2
 
 if [ -e .git ] ; then
 
-    git pull \
+    git checkout master && git pull origin master \
         && git submodule update --init \
         && git submodule update --remote \
         && cd 3rdparty/portable-target && git checkout master && git pull \
         && cd $CWD \
-        && cd 3rdparty/pfs/common && git checkout master && git pull  \
+        && cd 3rdparty/pfs/common && ./update.sh \
         && cd $CWD \
         && cd 3rdparty/cereal && git checkout $CEREAL_RELEASE
 
