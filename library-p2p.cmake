ca2737459194e353fdafe5fd5a2634a8da093ab2
Vladislav Trifochkin <demoversion@mail.ru>
2023-02-10 17:14:48 +0500

    fix: Fixed build scripts.

--- library-p2p.cmake
+++ library-p2p.cmake
@@ -7,22 +7,28 @@
 #      2021.06.21 Initial version.
 #      2021.06.22 Fixed completely.
 #      2023.01.17 Version 2.
+#      2023.02.10 Separated static and shared builds.
 ################################################################################
 cmake_minimum_required (VERSION 3.11)
 project(netty-p2p CXX)
 
+option(NETTY_P2P__BUILD_SHARED "Enable build shared library" ON)
+option(NETTY_P2P__BUILD_STATIC "Enable build static library" ON)
 option(NETTY_P2P__ENABLE_CEREAL "Enable cereal library (serialization backend)" ON)
 option(NETTY_P2P__ENABLE_CEREAL_THREAD_SAFETY "Enable cereal library thread safety" OFF)
 
-portable_target(ADD_SHARED ${PROJECT_NAME} ALIAS pfs::netty::p2p
-    EXPORTS NETTY__EXPORTS
-    BIND_STATIC ${PROJECT_NAME}-static
-    STATIC_ALIAS pfs::netty::p2p::static
-    STATIC_EXPORTS NETTY__STATIC)
+if (NOT PORTABLE_TARGET__CURRENT_PROJECT_DIR)
+    set(PORTABLE_TARGET__CURRENT_PROJECT_DIR ${CMAKE_CURRENT_SOURCE_DIR})
+endif()
 
-portable_target(INCLUDE_DIRS ${PROJECT_NAME} PUBLIC ${CMAKE_CURRENT_LIST_DIR}/include)
-portable_target(LINK ${PROJECT_NAME} PUBLIC pfs::netty)
-portable_target(LINK ${PROJECT_NAME}-static PUBLIC pfs::netty::static)
+if (NETTY_P2P__BUILD_SHARED)
+    portable_target(ADD_SHARED ${PROJECT_NAME} ALIAS pfs::netty::p2p EXPORTS NETTY__EXPORTS)
+endif()
+
+if (NETTY_P2P__BUILD_STATIC)
+    set(STATIC_PROJECT_NAME ${PROJECT_NAME}-static)
+    portable_target(ADD_STATIC ${STATIC_PROJECT_NAME} ALIAS pfs::netty::p2p::static EXPORTS NETTY__STATIC)
+endif()
 
 if (NETTY_P2P__ENABLE_CEREAL)
     if (NOT TARGET cereal)
@@ -30,21 +36,23 @@ if (NETTY_P2P__ENABLE_CEREAL)
     endif()
 
     portable_target(LINK ${PROJECT_NAME} PUBLIC cereal)
-    portable_target(LINK ${PROJECT_NAME}-static PUBLIC cereal)
+    portable_target(LINK ${STATIC_PROJECT_NAME} PUBLIC cereal)
 endif(NETTY_P2P__ENABLE_CEREAL)
 
-portable_target(SOURCES ${PROJECT_NAME}
+list(APPEND _netty_p2p__sources
     ${CMAKE_CURRENT_LIST_DIR}/src/p2p/file_transporter.cpp
     ${CMAKE_CURRENT_LIST_DIR}/src/p2p/posix/discovery_engine.cpp)
 
-#if (MSVC)
-    #portable_target(COMPILE_OPTIONS ${PROJECT_NAME} "/wd4251")
-    #portable_target(COMPILE_OPTIONS ${PROJECT_NAME}-static "/wd4251")
+if (NETTY_P2P__BUILD_SHARED)
+    portable_target(SOURCES ${PROJECT_NAME} ${_netty_p2p__sources})
+    portable_target(INCLUDE_DIRS ${PROJECT_NAME} PUBLIC ${CMAKE_CURRENT_LIST_DIR}/include)
+    portable_target(LINK ${PROJECT_NAME} PUBLIC pfs::netty)
+    portable_target(DEFINITIONS ${PROJECT_NAME} PRIVATE PFS__LOG_LEVEL=2)
+endif()
 
-    #portable_target(COMPILE_OPTIONS ${PROJECT_NAME} "/wd26812")
-    #portable_target(COMPILE_OPTIONS ${PROJECT_NAME}-static "/wd26812")
-#endif(MSVC)
-
-portable_target(DEFINITIONS ${PROJECT_NAME} PRIVATE PFS__LOG_LEVEL=2)
-portable_target(DEFINITIONS ${PROJECT_NAME}-static PRIVATE PFS__LOG_LEVEL=2)
-
+if (NETTY_P2P__BUILD_STATIC)
+    portable_target(SOURCES ${STATIC_PROJECT_NAME} ${_netty_p2p__sources})
+    portable_target(INCLUDE_DIRS ${STATIC_PROJECT_NAME} PUBLIC ${CMAKE_CURRENT_LIST_DIR}/include)
+    portable_target(LINK ${STATIC_PROJECT_NAME} PUBLIC pfs::netty::static)
+    portable_target(DEFINITIONS ${STATIC_PROJECT_NAME} PRIVATE PFS__LOG_LEVEL=2)
+endif()
--- library.cmake
+++ library.cmake
@@ -1,5 +1,5 @@
 ################################################################################
-# Copyright (c) 2021 Vladislav Trifochkin
+# Copyright (c) 2021-2023 Vladislav Trifochkin
 #
 # This file is part of `netty-lib`.
 #
@@ -7,26 +7,35 @@
 #      2021.06.21 Initial version.
 #      2021.06.22 Fixed completely.
 #      2022.01.14 Refactored for use `portable_target`.
+#      2023.02.10 Separated static and shared builds.
 ################################################################################
 cmake_minimum_required (VERSION 3.11)
 project(netty CXX)
 
 include(CheckIncludeFile)
 
+option(NETTY__BUILD_SHARED "Enable build shared library" ON)
+option(NETTY__BUILD_STATIC "Enable build static library" ON)
 option(NETTY__ENABLE_UDT "Enable modified UDT library (reliable UDP implementation)" ON)
 option(NETTY__UDT_PATCHED "Enable modified UDT library with patches" ON)
 option(NETTY__ENABLE_QT5 "Enable Qt5 library (network backend)" OFF)
 
-portable_target(ADD_SHARED ${PROJECT_NAME} ALIAS pfs::netty
-    EXPORTS NETTY__EXPORTS
-    BIND_STATIC ${PROJECT_NAME}-static
-    STATIC_ALIAS pfs::netty::static
-    STATIC_EXPORTS NETTY__STATIC)
+if (NOT PORTABLE_TARGET__CURRENT_PROJECT_DIR)
+    set(PORTABLE_TARGET__CURRENT_PROJECT_DIR ${CMAKE_CURRENT_SOURCE_DIR})
+endif()
 
-portable_target(DEFINITIONS ${PROJECT_NAME} PRIVATE PFS__LOG_LEVEL=2)
-portable_target(DEFINITIONS ${PROJECT_NAME}-static PRIVATE PFS__LOG_LEVEL=2)
+if (NETTY__BUILD_SHARED)
+    portable_target(ADD_SHARED ${PROJECT_NAME} ALIAS pfs::netty EXPORTS NETTY__EXPORTS)
+endif()
 
-portable_target(SOURCES ${PROJECT_NAME}
+if (NETTY__BUILD_STATIC)
+    set(STATIC_PROJECT_NAME ${PROJECT_NAME}-static)
+    portable_target(ADD_STATIC ${STATIC_PROJECT_NAME} ALIAS pfs::netty::static EXPORTS NETTY__STATIC)
+endif()
+
+list(APPEND _netty__definitions "PFS__LOG_LEVEL=2")
+
+list(APPEND _netty__sources
     ${CMAKE_CURRENT_LIST_DIR}/src/error.cpp
     ${CMAKE_CURRENT_LIST_DIR}/src/inet4_addr.cpp
     ${CMAKE_CURRENT_LIST_DIR}/src/socket4_addr.cpp
@@ -39,11 +48,11 @@ portable_target(SOURCES ${PROJECT_NAME}
     ${CMAKE_CURRENT_LIST_DIR}/src/posix/udp_sender.cpp)
 
 if (UNIX OR ANDROID)
-    portable_target(SOURCES ${PROJECT_NAME}
+    list(APPEND _netty__sources
         ${CMAKE_CURRENT_LIST_DIR}/src/network_interface.cpp
         ${CMAKE_CURRENT_LIST_DIR}/src/network_interface_linux.cpp)
 elseif (MSVC)
-    portable_target(SOURCES ${PROJECT_NAME}
+    list(APPEND _netty__sources
         ${CMAKE_CURRENT_LIST_DIR}/src/network_interface.cpp
         ${CMAKE_CURRENT_LIST_DIR}/src/network_interface_win32.cpp)
 else()
@@ -55,28 +64,28 @@ if (NOT TARGET pfs::common)
         ${CMAKE_CURRENT_LIST_DIR}/3rdparty/pfs/common/library.cmake)
 endif()
 
-portable_target(INCLUDE_DIRS ${PROJECT_NAME} PUBLIC ${CMAKE_CURRENT_LIST_DIR}/include)
-portable_target(LINK ${PROJECT_NAME} PUBLIC pfs::common)
-portable_target(LINK ${PROJECT_NAME}-static PUBLIC pfs::common)
-
 if (MSVC)
-    portable_target(COMPILE_OPTIONS ${PROJECT_NAME} PUBLIC "/wd4251" "/wd4267" "/wd4244")
-    portable_target(COMPILE_OPTIONS ${PROJECT_NAME}-static PUBLIC "/wd4251" "/wd4267" "/wd4244")
-    portable_target(LINK ${PROJECT_NAME} PRIVATE Ws2_32 Iphlpapi)
-    portable_target(LINK ${PROJECT_NAME}-static PRIVATE Ws2_32 Iphlpapi)
+    list(APPEND _netty__compile_options "/wd4251" "/wd4267" "/wd4244")
+
+    if (NETTY__BUILD_STATIC)
+        portable_target(LINK ${PROJECT_NAME} PRIVATE Ws2_32 Iphlpapi)
+    endif()
+
+    if (NETTY__BUILD_SHARED)
+        portable_target(LINK ${STATIC_PROJECT_NAME} PRIVATE Ws2_32 Iphlpapi)
+    endif()
 endif(MSVC)
 
 CHECK_INCLUDE_FILE("poll.h" __has_poll)
 
 if (__has_poll)
-    portable_target(SOURCES ${PROJECT_NAME}
+    list(APPEND _netty__sources
         ${CMAKE_CURRENT_LIST_DIR}/src/posix/connecting_poller.cpp
         ${CMAKE_CURRENT_LIST_DIR}/src/posix/listener_poller.cpp
         ${CMAKE_CURRENT_LIST_DIR}/src/posix/reader_poller.cpp
         ${CMAKE_CURRENT_LIST_DIR}/src/posix/poll_poller.cpp
         ${CMAKE_CURRENT_LIST_DIR}/src/posix/writer_poller.cpp)
-    portable_target(DEFINITIONS ${PROJECT_NAME} PUBLIC "NETTY__POLL_ENABLED=1")
-    portable_target(DEFINITIONS ${PROJECT_NAME}-static PUBLIC "NETTY__POLL_ENABLED=1")
+    list(APPEND _netty__definitions "NETTY__POLL_ENABLED=1")
 endif()
 
 if (NOT MSVC)
@@ -84,28 +93,26 @@ if (NOT MSVC)
 endif()
 
 if (MSVC OR __has_sys_select)
-    portable_target(SOURCES ${PROJECT_NAME}
+    list(APPEND _netty__sources
         ${CMAKE_CURRENT_LIST_DIR}/src/posix/connecting_poller.cpp
         ${CMAKE_CURRENT_LIST_DIR}/src/posix/listener_poller.cpp
         ${CMAKE_CURRENT_LIST_DIR}/src/posix/reader_poller.cpp
         ${CMAKE_CURRENT_LIST_DIR}/src/posix/select_poller.cpp
         ${CMAKE_CURRENT_LIST_DIR}/src/posix/writer_poller.cpp)
-    portable_target(DEFINITIONS ${PROJECT_NAME} PUBLIC "NETTY__SELECT_ENABLED=1")
-    portable_target(DEFINITIONS ${PROJECT_NAME}-static PUBLIC "NETTY__SELECT_ENABLED=1")
+    list(APPEND _netty__definitions "NETTY__SELECT_ENABLED=1")
 endif()
 
 if (UNIX)
     CHECK_INCLUDE_FILE("sys/epoll.h" __has_sys_epoll)
 
     if (__has_sys_epoll)
-        portable_target(SOURCES ${PROJECT_NAME}
+        list(APPEND _netty__sources
             ${CMAKE_CURRENT_LIST_DIR}/src/linux/connecting_poller.cpp
             ${CMAKE_CURRENT_LIST_DIR}/src/linux/epoll_poller.cpp
             ${CMAKE_CURRENT_LIST_DIR}/src/linux/listener_poller.cpp
             ${CMAKE_CURRENT_LIST_DIR}/src/linux/reader_poller.cpp
             ${CMAKE_CURRENT_LIST_DIR}/src/linux/writer_poller.cpp)
-        portable_target(DEFINITIONS ${PROJECT_NAME} PUBLIC "NETTY__EPOLL_ENABLED=1")
-        portable_target(DEFINITIONS ${PROJECT_NAME}-static PUBLIC "NETTY__EPOLL_ENABLED=1")
+        list(APPEND _netty__definitions "NETTY__EPOLL_ENABLED=1")
     endif()
 endif()
 
@@ -114,7 +121,7 @@ if (NETTY__ENABLE_UDT)
     set(NETTY__UDT_ROOT "${CMAKE_CURRENT_LIST_DIR}/src/udt/newlib")
 
     # UDT sources
-    portable_target(SOURCES ${PROJECT_NAME}
+    list(APPEND _netty__sources
         ${NETTY__UDT_ROOT}/api.cpp
         ${NETTY__UDT_ROOT}/buffer.cpp
         ${NETTY__UDT_ROOT}/cache.cpp
@@ -129,7 +136,7 @@ if (NETTY__ENABLE_UDT)
         ${NETTY__UDT_ROOT}/queue.cpp
         ${NETTY__UDT_ROOT}/window.cpp)
 
-    portable_target(SOURCES ${PROJECT_NAME}
+    list(APPEND _netty__sources
         ${CMAKE_CURRENT_LIST_DIR}/src/udt/connecting_poller.cpp
         ${CMAKE_CURRENT_LIST_DIR}/src/udt/epoll_poller.cpp
         ${CMAKE_CURRENT_LIST_DIR}/src/udt/listener_poller.cpp
@@ -139,26 +146,64 @@ if (NETTY__ENABLE_UDT)
         ${CMAKE_CURRENT_LIST_DIR}/src/udt/debug_CCC.cpp
         ${CMAKE_CURRENT_LIST_DIR}/src/udt/writer_poller.cpp)
 
-    portable_target(DEFINITIONS ${PROJECT_NAME} PUBLIC UDT_EXPORTS)
-    portable_target(DEFINITIONS ${PROJECT_NAME}-static PRIVATE UDT_STATIC)
+    if (NETTY__BUILD_SHARED)
+        list(APPEND _netty__definitions UDT_EXPORTS)
+    endif()
 
-    portable_target(DEFINITIONS ${PROJECT_NAME} PUBLIC "NETTY__UDT_ENABLED=1")
-    portable_target(DEFINITIONS ${PROJECT_NAME}-static PUBLIC "NETTY__UDT_ENABLED=1")
+    if (NETTY__BUILD_STATIC)
+        list(APPEND _netty__definitions UDT_STATIC)
+    endif()
+
+    list(APPEND _netty__definitions "NETTY__UDT_ENABLED=1")
 
     if (NETTY__UDT_PATCHED)
-        portable_target(DEFINITIONS ${PROJECT_NAME} PRIVATE "NETTY__UDT_PATCHED=1")
-        portable_target(DEFINITIONS ${PROJECT_NAME}-static PRIVATE "NETTY__UDT_PATCHED=1")
+        list(APPEND _netty__private_definitions "NETTY__UDT_PATCHED=1")
     endif(NETTY__UDT_PATCHED)
 endif(NETTY__ENABLE_UDT)
 
 if (NETTY__ENABLE_QT5)
-    portable_target(SOURCES ${PROJECT_NAME}
+    list(APPEND _netty__sources
         ${CMAKE_CURRENT_LIST_DIR}/src/qt5/udp_recever.cpp
         ${CMAKE_CURRENT_LIST_DIR}/src/qt5/udp_sender.cpp
         ${CMAKE_CURRENT_LIST_DIR}/src/qt5/udp_socket.cpp)
-
-    portable_target(LINK_QT5_COMPONENTS ${PROJECT_NAME} PUBLIC Core Network)
-    portable_target(LINK_QT5_COMPONENTS ${PROJECT_NAME}-static Core Network)
-    portable_target(DEFINITIONS ${PROJECT_NAME} PUBLIC "NETTY__QT5_ENABLED=1")
-    portable_target(DEFINITIONS ${PROJECT_NAME}-static PUBLIC "NETTY__QT5_ENABLED=1")
+    list(APPEND _netty__definitions "NETTY__QT5_ENABLED=1")
+    list(APPEND _netty__qt5_components Core Network)
 endif(NETTY__ENABLE_QT5)
+
+if (NETTY__BUILD_SHARED)
+    portable_target(SOURCES ${PROJECT_NAME} ${_netty__sources})
+    portable_target(DEFINITIONS ${PROJECT_NAME} PUBLIC ${_netty__definitions})
+    portable_target(INCLUDE_DIRS ${PROJECT_NAME} PUBLIC ${CMAKE_CURRENT_LIST_DIR}/include)
+    portable_target(LINK ${PROJECT_NAME} PUBLIC pfs::common)
+
+    if (_netty__qt5_components)
+        portable_target(LINK_QT5_COMPONENTS ${PROJECT_NAME} PRIVATE ${_netty__qt5_components})
+    endif()
+
+    if (_netty__private_definitions)
+        portable_target(DEFINITIONS ${PROJECT_NAME} PRIVATE ${_netty__private_definitions})
+    endif()
+
+    if (_netty__compile_options)
+        portable_target(COMPILE_OPTIONS ${PROJECT_NAME} PUBLIC ${_netty__compile_options})
+    endif()
+endif()
+
+if (NETTY__BUILD_STATIC)
+    portable_target(SOURCES ${STATIC_PROJECT_NAME} ${_netty__sources})
+    portable_target(DEFINITIONS ${STATIC_PROJECT_NAME} PUBLIC ${_netty__definitions})
+    portable_target(INCLUDE_DIRS ${STATIC_PROJECT_NAME} PUBLIC ${CMAKE_CURRENT_LIST_DIR}/include)
+    portable_target(LINK ${STATIC_PROJECT_NAME} PUBLIC pfs::common)
+
+    if (_netty__qt5_components)
+        portable_target(LINK_QT5_COMPONENTS ${STATIC_PROJECT_NAME} PRIVATE ${_netty__qt5_components})
+    endif()
+
+    if (_netty__private_definitions)
+        portable_target(DEFINITIONS ${STATIC_PROJECT_NAME} PRIVATE ${_netty__private_definitions})
+    endif()
+
+    if (_netty__compile_options)
+        portable_target(COMPILE_OPTIONS ${STATIC_PROJECT_NAME} PUBLIC ${_netty__compile_options})
+    endif()
+endif()
